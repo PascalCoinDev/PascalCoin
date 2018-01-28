f0c2ec8cfabd4f312c8cfaa4eeadd57591c8cb32
maciej-izak <hnb.code@gmail.com>
2018-01-28 23:25:00 +0100

    Update to latest Generics.Collections version

--- Units/Utils/generics.strings.pas
+++ Units/Utils/generics.strings.pas
@@ -25,6 +25,8 @@ interface
 resourcestring
   SArgumentOutOfRange = 'Argument out of range';
   SDuplicatesNotAllowed = 'Duplicates not allowed in dictionary';
+  SCollectionInconsistency = 'Collection inconsistency';
+  SCollectionDuplicate = 'Collection does not allow duplicates';
   SDictionaryKeyDoesNotExist = 'Dictionary key does not exist';
   SItemNotFound = 'Item not found';
 
