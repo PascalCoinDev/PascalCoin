a6ce3fe05d854058229a7a0ef6fcc5160fe50abd
Ugochukwu Mmaduekwe <ugo4brain@gmail.com>
2018-03-07 14:57:28 +0100

    update to previous fix for runtime error.

--- src/gui/UFRMMainForm.pas
+++ src/gui/UFRMMainForm.pas
@@ -70,7 +70,6 @@ type
     tbtnConnectivity: TToolButton;
     tbtnSync: TToolButton;
     tbtnWalletLock: TToolButton;
-    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCloseQuery(Sender: TObject; var CanClose: boolean);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
@@ -167,11 +166,6 @@ begin
     end;
     mbCancel: CanClose := false;
   end;
-end;
-
-procedure TFRMMainForm.FormClose(Sender: TObject; var CloseAction: TCloseAction);
-begin
-  // do not delete even if empty
 end;
 
 procedure TFRMMainForm.CM_Terminate(var Msg: TMessage);
