80154eac6a4e529cfd97c00d4b76a9eb8a298e00
PascalCoin <bpascalblockchain@gmail.com>
2019-03-12 20:30:15 +0100

    Revert "Allow duplicate connections on TESTNET (for local testing)"
    This reverts commit 8a947c998fb7d91285f340df4ebba132325c25aa.

--- src/core/UNetProtocol.pas
+++ src/core/UNetProtocol.pas
@@ -1425,7 +1425,6 @@ function TNetData.FindConnectionByClientRandomValue(Sender: TNetConnection): TNe
 Var l : TList<TNetConnection>;
   i : Integer;
 begin
-  {$IFNDEF TESTNET}
   l := FNetConnections.LockList;
   try
     for i := 0 to L.Count - 1 do begin
@@ -1435,7 +1434,6 @@ begin
   finally
     FNetConnections.UnlockList;
   end;
-  {$ENDIF}
   Result := Nil;
 end;
 
