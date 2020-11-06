4a61f1ee060cd296b91ce001d683f83a2f2c6630
Pascal Coin <bpascalblockchain@gmail.com>
2020-11-06 20:07:50 +0100

    Fix pascalcoin_daemon bug on Windows as a service
    Now can be installed as a service on windows calling pascalcoin_daemon -i (or -u to uninstall)

--- src/pascalcoin_daemon.pp
+++ src/pascalcoin_daemon.pp
@@ -11,11 +11,30 @@ uses
   Classes, daemonapp,
   UCrypto, upcdaemon;
 
+Type
+
+  { TDaemonMainLoop }
+
+  TDaemonMainLoop = Class
+  public
+    Class procedure DaemonMainLoop;
+  end;
+
+{ TDaemonMainLoop }
+
+class procedure TDaemonMainLoop.DaemonMainLoop;
+begin
+  while not Application.Terminated do begin
+    CheckSynchronize(10);
+  end;
+end;
 
 begin
   Application.Title:='PascalCoin Daemon application';
+  IsConsole:=False;
   RegisterDaemonClass(TPCDaemon);
   RegisterDaemonMapper(TPCDaemonMapper);
+  Application.GUIMainLoop:=@TDaemonMainLoop.DaemonMainLoop;
   TCrypto.InitCrypto;
   Application.Run;
 end.
