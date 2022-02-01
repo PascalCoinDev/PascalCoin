fb595cffa415c8d3f83229b3b33fe2bb0644df5f
Skybuck Flying <skybuck2000@hotmail.com>
2022-02-01 01:00:22 +0100

    CreateSpacesToTabsFilter.sh improved.

--- CreateSpacesToTabsFilter.sh
+++ CreateSpacesToTabsFilter.sh
@@ -16,6 +16,17 @@ echo http://gnuwin32.sourceforge.net/packages.html
 echo and here:
 echo http://gnuwin32.sourceforge.net/packages/coreutils.htm
 echo
+echo Recommend to install it to C:/Tools/GnuWin32
+echo Rename expand.exe to CoreUtilsExpand.exe
+echo Rename unexpand.exe to CoreUtilsUnExpand.exe
+echo
+echo In case the config commands had no effect on C:\Tools\Git\Etc\gitconfig
+echo then add these three lines manually to the global gitconfig file in \etc:
+echo
+echo [filter "tabspace2"]
+echo        clean = expand --tabs=2 --initial
+echo        smudge = unexpand --tabs=2 --first-only
+echo
 echo After installation its bin folder needs to be on the path environment
 echo So that expand.exe and unexpand.exe can be found and executed.
 echo
@@ -23,6 +34,10 @@ echo To run this script type: ./CreateSpacesToTabsFilter.sh
 echo
 echo Run it on the main folder containing the subfolder .git
 echo
+git config --global filter.tabspace2.clean 'CoreUtilsExpand --tabs=2 --initial'
+git config --global filter.tabspace2.smudge 'CoreUtilsUnExpand --tabs=2 --first-only'
+git config filter.tabspace2.clean 'CoreUtilsExpand --tabs=2 --initial'
+git config filter.tabspace2.smudge 'CoreUtilsUnExpand --tabs=2 --first-only'
 md .git
 md .git/info
 mkdir .git
